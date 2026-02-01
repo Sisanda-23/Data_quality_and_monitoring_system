3ddc13823883cd8dc98ced802fea2e3382ddb3fb
Sisanda <thobileaby23@gmail.com>
2026-02-01 11:26:41 +0200

    Update validation_queries.sql

--- sql/validation_queries.sql
+++ sql/validation_queries.sql
@@ -1,1 +1,16 @@
+-- Check duplicates
+SELECT transaction_id, COUNT(*)
+FROM sales
+GROUP BY transaction_id
+HAVING COUNT(*) > 1;
+
+-- Check missing sales
+SELECT COUNT(*) 
+FROM sales
+WHERE sale_amount IS NULL;
+
+-- Outlier check
+SELECT *
+FROM sales
+WHERE sale_amount > 1000;
 
